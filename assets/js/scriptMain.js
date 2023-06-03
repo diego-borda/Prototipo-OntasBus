d7fa53231675e61e5e8e13bd424242f1cbc03df7
Diego Borda <diegoborda0508@gmail.com>
2023-06-02 22:45:43 -0600

    Toggle buttons function added: scriptMain.js (not verified)

---
+++ assets/js/scriptMain.js
@@ -0,0 +1,43 @@
+// Añadimos una clase "oculto" al menú lateral por defecto
+// const menuLateral = document.getElementById("mpp_Lateral");
+// const botonToggle = document.getElementById("botonToggle"); // Agrega un ID al botón
+
+// botonToggle.addEventListener("click", function() {
+//     menuLateral.classList.toggle("oculto");
+// });
+
+
+
+
+
+//menuLateral.style.width = '0';
+//headerContenido.style.width = '100%';
+
+/* El anterior es el correcto
+
+// Añadimos un evento click al botón
+botonToggle.addEventListener('click', () => {
+  // Alternamos la clase "oculto" del menú lateral al hacer clic en el botón
+  menuLateral.classList.toggle('oculto');
+  
+  // Si el menú lateral está visible
+  if (!menuLateral.classList.contains('oculto')) {
+    menuLateral.style.width = '30%';
+    headerContenido.style.width = '70%';
+  } else {
+    // Si el menú lateral está oculto
+    menuLateral.style.width = '0';
+    headerContenido.style.width = '100%';
+  }
+});
+*/
+
+
+  /*-----------------------Descomentar mas tarde ---------------------- 
+  const btnToggle= document.querySelector(".toggle-btn");
+
+  btnToggle.addEventListener('click', function (){
+
+     document.getElementById('sidebar').classList.toggle('active');
+
+ });*/
