c4b52680528ab801cd9e032c1301f6abfc8bba89
Diego Borda <diegoborda0508@gmail.com>
2023-06-05 20:21:18 -0600

    Stable version of header & lateral menu transition

---
+++ pruebas/MiPropiaPrueba.js
@@ -0,0 +1,37 @@
+// Añadimos una clase "oculto" al menú lateral por defecto
+ const menuLateral = document.getElementById("mpp_Lateral");
+ const flechaAtras = document.querySelector(".fa-angle-double-left"); // Agrega un ID al botón
+
+ flechaAtras.addEventListener("click", function() {
+     menuLateral.classList.toggle("oculto");
+ });
+
+ const flechaAdelante = document.querySelector(".fa-angle-double-right").addEventListener("click",function(){
+  menuLateral.classList.remove("oculto")
+ });
+
+
+
+
+
+//menuLateral.style.width = '0';
+//headerContenido.style.width = '100%';
+
+/*
+
+// Añadimos un evento click al botón
+botonToggle.addEventListener('click', () => {
+  // Alternamos la clase "oculto" del menú lateral al hacer clic en el botón
+  menuLateral.classList.toggle('oculto');
+  
+  // Si el menú lateral está visible
+  if (!menuLateral.classList.contains('oculto')) {
+    menuLateral.style.width = '30%';
+    headerContenido.style.width = '70%';
+  } else {
+    // Si el menú lateral está oculto
+    menuLateral.style.width = '0';
+    headerContenido.style.width = '100%';
+  }
+});
+*/
