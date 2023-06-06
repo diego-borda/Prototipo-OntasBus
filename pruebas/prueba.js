f9e57cad01f48e2091fcb3112736a82db3b64faf
Diego Borda <diegoborda0508@gmail.com>
2023-06-06 00:13:58 -0600

    1st Version: MenuLateral & Header (success)

---
+++ pruebas/prueba.js
@@ -0,0 +1,16 @@
+
+
+
+/*botonFlecha.addEventListener("click", function(){
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('open');
+    console.log("Funciona");
+})
+*/
+
+/*function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('open');
+    console.log("Funciona");
+  }*/
+  
