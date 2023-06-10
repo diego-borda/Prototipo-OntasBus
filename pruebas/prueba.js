c40bec31f58bc74a2699560f67c8ba9d63ece2c9
Diego Borda <diegoborda0508@gmail.com>
2023-06-09 21:01:18 -0600

    Subemnu added: Cuenta & History

--- pruebas/prueba.js
+++ pruebas/prueba.js
@@ -1,15 +1,27 @@
+// Ocultar submenús por defecto
+document.querySelector(".Submenu_Cuenta").style.display = "none";
+document.querySelector(".SubMenu_History").style.display = "none";
 
+// Obtener los elementos de los enlaces de los menús
+var miCuentaLink = document.getElementById("miCuentaLink");
+var historialLink = document.getElementById("historialLink");
 
-/*botonFlecha.addEventListener("click", function(){
-    var navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('open');
-    console.log("Funciona");
-})
-*/
+// Mostrar Submenu_Cuenta al hacer clic en "Mi Cuenta"
+miCuentaLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  document.querySelector(".Submenu_Cuenta").style.display = "block";
+  event.stopPropagation();
+});
 
-/*function toggleMenu() {
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('open');
-    console.log("Funciona");
-  }*/
-  
+// Mostrar SubMenu_History al hacer clic en "Historial"
+historialLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  document.querySelector(".SubMenu_History").style.display = "block";
+  event.stopPropagation();
+});
+
+// Ocultar submenús al hacer clic en cualquier lugar de la pantalla
+document.addEventListener("click", function() {
+  document.querySelector(".Submenu_Cuenta").style.display = "none";
+  document.querySelector(".SubMenu_History").style.display = "none";
+});
