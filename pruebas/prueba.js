3c71f5737758913bb187eaa552b811cb80465396
Diego Borda <diegoborda0508@gmail.com>
2023-06-10 20:34:55 -0600

    Function added: Cerrar Sesion -> prueba.js

--- pruebas/prueba.js
+++ pruebas/prueba.js
@@ -1,6 +1,17 @@
-// Ocultar submenús por defecto
+/*------------------ Funcionalidad Mostrar y Ocultar SubMenus -------------------------*/
+
+/*------------------ Ocultar submenús por defecto -------------------*/
 document.querySelector(".Submenu_Cuenta").style.display = "none";
 document.querySelector(".SubMenu_History").style.display = "none";
+
+/*--------------------- Ocultar todos los SubMenu al clickear -------------------------*/
+document.addEventListener("click", function() {
+  document.querySelector(".Submenu_Cuenta").style.display = "none";
+  document.querySelector(".SubMenu_History").style.display = "none";
+});
+
+/*----------------------------- SubMenu Cuenta -----------------------------*/
+
 
 // Obtener los elementos de los enlaces de los menús
 var miCuentaLink = document.getElementById("miCuentaLink");
@@ -13,15 +24,23 @@ miCuentaLink.addEventListener("click", function(event) {
   event.stopPropagation();
 });
 
+/*--------------------- Sub_SubMenu Historial -------------------------*/
 // Mostrar SubMenu_History al hacer clic en "Historial"
 historialLink.addEventListener("click", function(event) {
   event.preventDefault();
-  document.querySelector(".SubMenu_History").style.display = "block";
+  //Tal vez hay que cambiar a SOLAMENTE style.display = "blocks";
+  document.querySelector(".SubMenu_History").style.display = "flex";
+  document.querySelector(".SubMenu_History").style["flex-direction"] = "row";
   event.stopPropagation();
 });
 
-// Ocultar submenús al hacer clic en cualquier lugar de la pantalla
-document.addEventListener("click", function() {
-  document.querySelector(".Submenu_Cuenta").style.display = "none";
-  document.querySelector(".SubMenu_History").style.display = "none";
+
+
+
+/*------------------ Funcionalidad de Cerrar Sesion -------------------------*/
+document.getElementById("CerrarSesion").addEventListener("click", function() {
+  var confirmacion = confirm("¿Estás seguro de que deseas cerrar sesión?");
+  if (confirmacion) {
+    window.location.href = ".. Login.html";
+  }
 });
