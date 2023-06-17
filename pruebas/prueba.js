eca4fd13b7f3942e24246d6dd73c61f243aec2e2
Diego Borda <diegoborda0508@gmail.com>
2023-06-17 16:36:46 -0600

    Mapa code Deleted

--- pruebas/prueba.js
+++ pruebas/prueba.js
@@ -1,14 +1,3 @@
-/*------------------------------------- Mapa ----------------------------------------------*/
-/*--------------- Creacion de mapa ---------------*/
-var mapa=L.map("Mapa").setView([12.154372,-86.273],15)
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(mapa);
-// Segun el video esto queda hasta ... .png?,{}) --> asi
-
-/*-------------------- Creando marcadores -------------------*/
-var marcador=L.marker()
-
-
-
 /*------------------ Funcionalidad Mostrar y Ocultar SubMenus -------------------------*/
 
 /*------------------ Ocultar submenús HEADER por defecto -------------------*/
